7a4f1817e60146d2eb055941f5a2e9ffe3fe7f53
JuanDiego17 <juanorrdonez@gmail.com>
2021-02-16 19:57:21 -0500

    Script Inicial de la Base de Datos

---
+++ DB.sql
@@ -0,0 +1,70 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.4.14-MariaDB : Database - proyecto
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`proyecto` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `proyecto`;
+
+/*Table structure for table `reporte` */
+
+DROP TABLE IF EXISTS `reporte`;
+
+CREATE TABLE `reporte` (
+  `id_reporte` int(11) NOT NULL AUTO_INCREMENT,
+  `tabla` varchar(50) NOT NULL,
+  `accion` varchar(20) NOT NULL,
+  `registro` varchar(100) NOT NULL,
+  `registro_ant` varchar(100) NOT NULL,
+  `usuario` varchar(100) NOT NULL,
+  `fecha` date NOT NULL,
+  `usuario_mod` varchar(100) NOT NULL,
+  `fecha_mod` date NOT NULL,
+  PRIMARY KEY (`id_reporte`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `reporte` */
+
+/*Table structure for table `usuario` */
+
+DROP TABLE IF EXISTS `usuario`;
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `usuario` */
+
+/* Trigger structure for table `usuario` */
+
+DELIMITER $$
+
+/*!50003 DROP TRIGGER*//*!50032 IF EXISTS */ /*!50003 `SP_Insertar_Usuario` */$$
+
+/*!50003 CREATE */ /*!50017 DEFINER = 'root'@'localhost' */ /*!50003 TRIGGER `SP_Insertar_Usuario` AFTER INSERT ON `usuario` FOR EACH ROW BEGIN
+	INSERT INTO reporte(tabla,accion,registro,usuario,fecha)
+	VALUES ("Usuario","Insertar",new.nombre,USER(),NOW());
+    END */$$
+
+
+DELIMITER ;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
